493e98390724d4a217c3db0093139d85542ffc8e
sph-668 <sophiasmile48@gmail.com>
2020-07-15 23:43:17 +0700

    Changed number->phone

--- src/scripts/osint/region_check/__main__.py
+++ src/scripts/osint/region_check/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import requests
 from src.core.base.osint import OsintRunner, PossibleKeys
 from src.core.utils.response import ScriptResponse
@@ -10,13 +11,15 @@ class Runner(OsintRunner):
         super(Runner, self).__init__(logger)
 
     @staticmethod
-    def tel_region(number: str = "Unknown") -> str:
+    def __phone_to_region(phone: str = "Unknown") -> str:
         """
         Region recognition using the phone number
-        :param number: phone number to check
+        :param phone: phone number to check
         :return: information about the region and the operator
         """
-        response = requests.get('https://eduscan.net/help/phone_ajax.php?', params={'num': number})
+        response = requests.get(
+            "https://eduscan.net/help/phone_ajax.php", params={"num": phone}
+        )
         return response.text
 
     @validate_kwargs(PossibleKeys.KEYS)
@@ -27,12 +30,16 @@ class Runner(OsintRunner):
         :param kwargs: kwargs from core runner
         :return: ScriptResponse message
         """
-        result = self.tel_region(number=kwargs.get("number"))
-        if len(result) == 15:
-            return ScriptResponse.error(message="Sorry, no such number!")
+        try:
+            result = self.__phone_to_region(phone=kwargs.get("phone"))
+        except Exception:
+            return ScriptResponse.error(result=None, message="Something went wrong!")
+        result = result.split("~")
+        if result[1] != "0":
+            return ScriptResponse.success(result=None, message="Sorry, no such number!")
         return ScriptResponse.success(
-            result=result.split('~')[-1],
-            message=f"Successfully finished! (args: {args}, kwargs: {kwargs})",
+            result=(result[-1], result[-2]),
+            message=f"Found region: {result[-1]} | found operator: {result[-2]} | for phone number {result[0]}",
         )
 
 
