ed5617085188ea52685431a29ea77c6c3cd1e265
Minerm <minerm@mail.ru>
2020-07-15 14:51:20 +0700

    add script (email_verifier)

---
+++ src/scripts/osint/email_verifier/__main__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from src.core.base.osint import OsintRunner, PossibleKeys
+from src.core.utils.response import ScriptResponse
+from src.core.utils.validators import validate_kwargs
+from verify_email import verify_email
+
+class Runner(OsintRunner):
+
+    def __init__(self, logger: str = __name__):
+        super(Runner, self).__init__(logger)
+
+    @validate_kwargs(PossibleKeys.KEYS)
+    def run(self, *args, **kwargs)-> ScriptResponse.success or ScriptResponse.error:
+        email = kwargs.get("email")
+        result = verify_email(email)
+        if not result:
+            return ScriptResponse.success(result=result, message=f"Sorry, {email} is not exist :-( ")
+        return ScriptResponse.success(
+            result=result, message=f"Email {email} is exist!"
+        )
