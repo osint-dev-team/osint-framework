513b800f57ee87c8e48264bb25ee74e3bbec0680
Enhisir <enhisir@gmail.com>
2020-07-15 19:55:25 +0500

    Made simple email generator, modified requirements.txt

---
+++ src/scripts/osint/email_generator/__main__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from src.scripts.osint.email_generator import EmailGenerator
+from src.core.base.osint import OsintRunner, PossibleKeys
+from src.core.utils.response import ScriptResponse
+from src.core.utils.validators import validate_kwargs
+
+
+class Runner(OsintRunner):
+    def __init__(self, logger: str = __name__):
+        super(Runner, self).__init__(logger)
+
+    @validate_kwargs(PossibleKeys.KEYS)
+    def run(self, *args, **kwargs) -> ScriptResponse.success or ScriptResponse.error:
+        """
+        Main runner function for the script
+        :param args: args from core runner
+        :param kwargs: kwargs from core runner
+        :return: ScriptResponse message
+        """
+        try:
+            # to work normally you need to give a limit, because max size may be over a billion
+            # to reduce the value, you can help me better dividing username into lexems
+            emailgen = EmailGenerator(limit=100_000)
+            result = list(emailgen.Generate(kwargs.get("username")))
+        except Exception as err:
+            ScriptResponse.error(message=str(err))
+        else:
+            return ScriptResponse.success(
+                result=result, message="Successfully finished!"
+            )
+
+
+# if __name__ =="__main__":
+#     script_module = Runner()
+#     script_result = script_module.run(username="matveysergeev")
+#     print(script_result)
