c01df899ffb19a589a3aa1bd3880157077d20288
Timur Kasimov <timurcat7@gmail.com>
2020-07-16 18:20:59 +0700

    Added dosctring.

--- src/scripts/osint/ip_info/get_ip_info.py
+++ src/scripts/osint/ip_info/get_ip_info.py
@@ -9,6 +9,20 @@ BASE_URL = 'http://ip-api.com/json/{query}?fields=18411513'
 
 def get_ip_info(ip: str) -> dict or str:
     """
+    A function for retrieving location and provider info by IP address. You can get the following information:
+    - continent name
+    - country name
+    - region/state name
+    - city name
+    - district name
+    - zip code
+    - latitude
+    - longitude
+    - timezone
+    - ISP name
+    - organization name
+    - AS number and organization, separated by space (RIR). Empty for IP blocks not being announced in BGP tables.
+    - hosting, colocated or data center.
 
     :param ip: the IP address you want to know about.
     :return: string with error message in case of error or a dict with information about IP address.
