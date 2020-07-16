d14cfc87f892838f794f9bcd4659ca37f9656bdb
Timur Kasimov <timurcat7@gmail.com>
2020-07-16 18:13:53 +0700

    Added IP address as one of the possible keys.

--- src/core/base/osint.py
+++ src/core/base/osint.py
@@ -14,7 +14,7 @@ class PossibleKeys:
     Defines default values for the function arguments (kwargs, named args)
     """
 
-    KEYS = ["email", "username", "fullname", "vk_api_key", "phone"]
+    KEYS = ["email", "username", "fullname", "vk_api_key", "phone", "ip"]
 
 
 class OsintRunner(BaseRunner):
