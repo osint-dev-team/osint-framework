2907b0f313aea827842e87ebbf5f1cadb8d0fe71
manmolecular <regwebghost@yandex.ru>
2020-07-16 14:49:08 +0700

    Little fixes

--- src/scripts/osint/email_verifier/__main__.py
+++ src/scripts/osint/email_verifier/__main__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+from verify_email import verify_email
+
 from src.core.base.osint import OsintRunner, PossibleKeys
 from src.core.utils.response import ScriptResponse
 from src.core.utils.validators import validate_kwargs
-from verify_email import verify_email
 
 
 class Runner(OsintRunner):
@@ -12,11 +13,10 @@ class Runner(OsintRunner):
 
     @validate_kwargs(PossibleKeys.KEYS)
     def run(self, *args, **kwargs) -> ScriptResponse.success or ScriptResponse.error:
-        kwargs = {"email": "johndoe@gmail.com"}
         email = kwargs.get("email")
         result = verify_email(email)
         if not result:
             return ScriptResponse.success(
-                result=result, message=f"Sorry, email {email} does not exist :-( "
+                result=result, message=f"Sorry, email {email} does not exist"
             )
-        return ScriptResponse.success(result=result, message=f"Email {email} exist!")
+        return ScriptResponse.success(result=result, message=f"Email {email} exists")
