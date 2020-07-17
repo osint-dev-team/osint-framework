9c0d0a1f87f5c44d9cf8ccd97c526e28029501c3
Timur Kasimov <timurcat7@gmail.com>
2020-07-17 16:39:34 +0700

    Reworked exception handling mechanism.

--- src/scripts/osint/ip_info/__main__.py
+++ src/scripts/osint/ip_info/__main__.py
@@ -7,29 +7,27 @@ from src.core.base.recon import ReconRunner, PossibleKeys
 from src.core.utils.response import ScriptResponse
 from src.core.utils.validators import validate_kwargs
 
-BASE_URL = 'http://ip-api.com/json/{query}?fields=18411513'
-
 
 class Runner(ReconRunner):
     def __init__(self, logger: str = __name__) -> None:
         super(Runner, self).__init__(logger)
+        self.__BASE_URL = 'http://ip-api.com/json/{query}?fields=18411513'
 
     @validate_kwargs(PossibleKeys.KEYS)
     def run(self, *args, **kwargs) -> ScriptResponse.success or ScriptResponse.error:
         response = self.__get_ip_info(kwargs.get('ip'))
 
-        if type(response) == dict:
+        try:
             msg = 'Query successful!' if response['status'] == 'success' else 'Query failed!'
             response.pop('status', None)  # we don't need duplicate status, let's get rid of it
 
             result = ScriptResponse.success(result=response, message=msg)
-        else:
+        except TypeError:
             result = ScriptResponse.error(result=response, message='Error occurred while trying to get data!')
 
         return result
 
-    @staticmethod
-    def __get_ip_info(ip: str):
+    def __get_ip_info(self, ip: str) -> dict or str:
         """
             A function for retrieving location and provider info by IP address. You can get the following information:
             - continent name
@@ -57,20 +55,15 @@ class Runner(ReconRunner):
             return 'Invalid IP address!'
 
         try:
-            ipaddress.ip_address(ip)
-
-            try:
-                response = requests.get(BASE_URL.format(query=ip)).json()
-            except HTTPError as err_http:
-                response = 'HTTP error occurred: {}'.format(err_http)
-            except ConnectionError as err_conn:
-                response = 'Connection error occurred: {}'.format(err_conn)
-            except Timeout as err_timeout:
-                response = 'Timeout error occurred: {}'.format(err_timeout)
-            except RequestException as err:
-                response = 'Catastrophic error occurred: {}'.format(err)
-        except ValueError:
-            response = 'Invalid IP address!'
+            response = requests.get(self.__BASE_URL.format(query=ip)).json()
+        except HTTPError as err_http:
+            response = 'HTTP error occurred: {}'.format(err_http)
+        except ConnectionError as err_conn:
+            response = 'Connection error occurred: {}'.format(err_conn)
+        except Timeout as err_timeout:
+            response = 'Timeout error occurred: {}'.format(err_timeout)
+        except RequestException as err:
+            response = 'Catastrophic error occurred: {}'.format(err)
 
         return response
 
