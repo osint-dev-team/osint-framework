777c17302735dad6308dfd87ca98f15e1c31fd9e
manmolecular <regwebghost@yandex.ru>
2020-07-18 01:05:49 +0700

    Fix module runner a bit

--- src/core/utils/module.py
+++ src/core/utils/module.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
-from sys import argv
-from src.core.base import base
 
-
-def run_module(module: base, args: list, arg_name: str, arg_value: str or None = None) -> dict:
+def run_module(runner: type, args: list, arg_name: str, arg_default: str or None = None) -> dict:
     """
     Use module as a 'python3 -m ...' module
-    :param module: module object
+    :param runner: runner object
     :param args: list of args from CLI
     :param arg_name: name of arg to use
-    :param arg_value: default arg value
+    :param arg_default: default arg value
     :return: results
     """
-    runner = module.Runner()
+    runner = runner()
     try:
         arg_value = args[1]
     except:
         pass
-    return runner.run(**{arg_name: arg_value})
+    return runner.run(**{arg_name: arg_default})
