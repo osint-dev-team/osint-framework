11a5fabb2627ea21e3f245bdab54065b49b7a352
Iandmee <milaevpro1@gmail.com>
2020-07-20 14:29:51 +0300

    add __init__.py,__main__.py,module.py,test_module.py

--- src/scripts/recon/get_title/__main__.py
+++ src/scripts/recon/get_title/__main__.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python3
-import requests
 
-from src.core.base.recon import ReconRunner
-from src.core.utils.response import ScriptResponse
-from src.core.utils.validators import validate_kwargs
+from pprint import pprint
+from sys import argv
 
+from src.core.utils.module import run_module
+from .module import Runner
 
-class Runner(ReconRunner):
-    def __init__(self, logger: str = __name__):
-        super(Runner, self).__init__(logger)
-
-    def run(self, *args, **kwargs):
-        """
-        Try to find http title
-        :param args: args from core runner
-        :param kwargs: kwargs from core runner
-        :return: http title if it's exist
-        """
-        url = kwargs.get("url")
-        response = requests.get(url)
-        response = response.text
-        result = "None title"
-        try:
-            result = response[response.find('<title>') + 7: response.find('</title>')]
-        except:
-            pass
-        return ScriptResponse.success(
-            result=result, message="Title on {url} is: ".format(url=url)
-        )
+result = run_module(
+    Runner, args=argv, arg_name="url", arg_default="johndoe@gmail.com"
+)
+pprint(result)
