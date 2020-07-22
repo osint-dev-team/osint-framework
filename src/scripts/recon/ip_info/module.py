97ee61f8d34fcbe335e78cb39a019ec6728f5bc2
manmolecular <regwebghost@yandex.ru>
2020-07-23 01:01:39 +0700

    Apply Black linter on the main module file

--- src/scripts/recon/ip_info/module.py
+++ src/scripts/recon/ip_info/module.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import ipaddress
 
 import requests
@@ -10,19 +12,25 @@ from src.core.utils.validators import validate_kwargs
 class Runner(ReconRunner):
     def __init__(self, logger: str = __name__) -> None:
         super(Runner, self).__init__(logger)
-        self.__BASE_URL = 'http://ip-api.com/json/{query}?fields=18411513'
+        self.__BASE_URL = "http://ip-api.com/json/{query}?fields=18411513"
 
     @validate_kwargs(PossibleKeys.KEYS)
     def run(self, *args, **kwargs) -> ScriptResponse.success or ScriptResponse.error:
-        response = self.__get_ip_info(kwargs.get('ip'))
+        # fmt: off
+        response = self.__get_ip_info(kwargs.get("ip"))
 
         try:
-            msg = 'Query successful!' if response['status'] == 'success' else 'Query failed!'
-            response.pop('status', None)  # we don't need duplicate status, let's get rid of it
-
+            msg = (
+                "Query successful!"
+                if response["status"] == "success"
+                else "Query failed!"
+            )
+            # we don't need duplicate status, let's get rid of it
+            response.pop("status", None)
             return ScriptResponse.success(result=response, message=msg)
         except TypeError as type_err:
-            return ScriptResponse.error(message='Error occurred while trying to get data: {}'.format(str(type_err)))
+            return ScriptResponse.error(message=f"Error occurred while trying to get data: {str(type_err)}")
+        # fmt: on
 
     def __get_ip_info(self, ip: str) -> dict or str:
         """
@@ -49,9 +57,9 @@ class Runner(ReconRunner):
         try:
             validated_ip = str(ipaddress.ip_address(ip))
         except ValueError:
-            return 'Invalid IP address!'
+            return "Invalid IP address!"
 
         try:
             return requests.get(self.__BASE_URL.format(query=validated_ip)).json()
         except Exception as err_unexp:
-            return 'Unexpected error occurred: {}'.format(str(err_unexp))
+            return "Unexpected error occurred: {}".format(str(err_unexp))
