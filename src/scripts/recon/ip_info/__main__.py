dbac77140281ec2ce5e785c03e12a863003fc806
manmolecular <regwebghost@yandex.ru>
2020-07-23 00:59:57 +0700

    Set default IP to Google DNS Resolver address (to be more stable)

--- src/scripts/recon/ip_info/__main__.py
+++ src/scripts/recon/ip_info/__main__.py
@@ -5,7 +5,7 @@ from src.core.utils.module import run_module
 from .module import Runner
 
 result = run_module(
-    Runner, args=argv, arg_name="ip", arg_default="24.0.0.1"
+    Runner, args=argv, arg_name="ip", arg_default="8.8.8.8"
 )
 
 pprint(result)
