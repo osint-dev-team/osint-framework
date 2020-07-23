77173b89cedca34920c73968a4335f1875079ae0
manmolecular <regwebghost@yandex.ru>
2020-07-23 20:34:07 +0700

    Add more info about case to the manager

--- main.py
+++ main.py
@@ -2,20 +2,19 @@
 
 """
 Main runner.
-
-Please, for all the searches, use the following classes:
-'BaseCase', 'OsintCase', 'ReconCase'
 """
 
-from src.core.runner.manager import CaseManager
 from pprint import pprint
 
+from src.core.runner.manager import CaseManager
 
-if __name__ == "__main__":
-    # fmt: off
-    cases = [
+
+class DefaultValues:
+    CASES = [
         {
             "case": "base",
+            "name": "testname-profile",
+            "description": "Base example for 'testname' user profile",
             "kwargs": {
                 "username": "testname",
                 "email": "testmail@gmail.com",
@@ -24,6 +23,8 @@ if __name__ == "__main__":
         },
         {
             "case": "osint",
+            "name": "johndoe-profile",
+            "description": "Osint example for 'johndoe' user profile",
             "kwargs": {
                 "username": "johndoe",
                 "email": "johndoe@gmail.com",
@@ -32,39 +33,58 @@ if __name__ == "__main__":
         },
         {
             "case": "recon",
+            "name": "facebook-website",
+            "description": "Recon example for 'facebook.com' website",
             "kwargs": {
                 "url": "https://facebook.com",
             }
         },
         {
             "case": "recon",
+            "name": "habr-website",
+            "description": "Recon example for 'habr.com' website",
             "kwargs": {
                 "url": "https://habr.com",
             }
         },
         {
             "case": "recon",
+            "name": "8-8-8-8-host",
+            "description": "Recon example for '8.8.8.8' host",
             "kwargs": {
                 "ip": "8.8.8.8"
             }
         },
         {
             "case": "recon",
-            "kwargs": {
-                "url": "https://habr.com",
-            }
-        },
-        {
-            "case": "recon",
+            "name": "13-91-95-74-host",
+            "description": "Recon example for '13.91.95.74' host",
             "kwargs": {
                 "ip": "13.91.95.74"
             }
         },
     ]
 
-    manager = CaseManager(cases=cases, max_workers=10)
-    results = manager.multi_case_runner()
-    for result in results:
-        pprint(result)
 
+if __name__ == "__main__":
+    # fmt: off
+
+    # Define CaseManager class
+    manager = CaseManager(cases=DefaultValues.CASES, max_workers=10)
+
+    # Run all the cases in parallel way
+    multiple_results = manager.multi_case_runner()
+    for single_result in multiple_results:
+        pprint(single_result)
+
+    # Run single case
+    single_results = manager.single_case_runner(
+        case_class="recon",
+        case_name="test single runner",
+        case_description="Nothing special",
+        url="https://habr.com/"
+    )
+
+    # Show results
+    pprint(single_results)
     # fmt: on
--- src/core/runner/manager.py
+++ src/core/runner/manager.py
@@ -7,10 +7,9 @@ Defines multiprocessing manager
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 
+from src.core.case.base import BaseCase
 from src.core.case.osint import OsintCase
 from src.core.case.recon import ReconCase
-from src.core.case.base import BaseCase
-
 from src.core.utils.log import Logger
 
 logger = Logger.get_logger(name=__name__)
@@ -20,11 +19,12 @@ class CaseManager:
     """
     Defines multiprocessing manager for cases
     """
+
     MAPPING = {
         "recon": ReconCase,
         "osint": OsintCase,
         "base": BaseCase,
-        "default": BaseCase
+        "default": BaseCase,
     }
 
     def __init__(self, cases: list or None = None, max_workers: int = 3):
@@ -36,20 +36,38 @@ class CaseManager:
         self.cases = cases
         self.max_workers = max_workers
 
-    def single_case_runner(self, case_name: str, *args, **kwargs):
+    def single_case_runner(
+        self,
+        case_class: str,
+        case_name: str or None = None,
+        case_description: str or None = None,
+        *args,
+        **kwargs,
+    ):
         """
         Define and smoke run the BaseCase
-        :param case_name: original class of the case, name of it
+        :param case_class: original class of the case, name of it
+        :param case_name: name of the case, can be any string
+        :param case_description: description of the case
         :param args: some args
         :param kwargs: some kwargs
         :return: result of the execution
         """
-        logger.info(f"start {self.MAPPING.get(case_name, 'default').__name__} case processing")
-        case = self.MAPPING.get(case_name, "default")()
+        logger.info(
+            f"start case '{case_name}' processing (class: '{self.MAPPING.get(case_class, 'default').__name__}')"
+        )
+        case = self.MAPPING.get(case_class, "default")()
         case.process(*args, **kwargs)
-        return case.get_results()
+        return {
+            "case_class": self.MAPPING.get(case_class, "default").__name__,
+            "case_name": case_name,
+            "case_description": case_description,
+            "case_results": case.get_results() or {},
+        }
 
-    def multi_case_runner(self, cases: list or None = None, max_workers: int or None = None):
+    def multi_case_runner(
+        self, cases: list or None = None, max_workers: int or None = None
+    ):
         """
         Run multiple case in separated processes
         :param cases: cases to run
@@ -57,15 +75,19 @@ class CaseManager:
         :return: results
         """
         futures = []
-        with ProcessPoolExecutor(max_workers=max_workers or self.max_workers) as executor:
+        with ProcessPoolExecutor(
+            max_workers=max_workers or self.max_workers
+        ) as executor:
             for case in cases or self.cases:
                 futures.append(
                     executor.submit(
                         partial(
                             self.single_case_runner,
-                            case_name=case.get("case"),
+                            case_class=case.get("case"),
+                            case_name=case.get("name"),
+                            case_description=case.get("description"),
                             *case.get("args", []),
-                            **case.get("kwargs", {})
+                            **case.get("kwargs", {}),
                         )
                     )
                 )
