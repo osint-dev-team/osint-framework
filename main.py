1816cdb13fec1b5f38d494247205f5c7e4208cd5
manmolecular <regwebghost@yandex.ru>
2020-07-30 02:05:17 +0700

    Add base scenario, add yaml support

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Main runner.
 
 from logging import basicConfig, INFO
 from pprint import pprint
+from yaml import safe_load
+from sys import argv
 
 from src.core.runner.manager import CaseManager
 from src.core.utils.log import Logger
@@ -15,82 +17,56 @@ logger = Logger.get_logger(name="osint-framework")
 
 
 class DefaultValues:
-    CASES = [
-        {
-            "case": "base",
-            "name": "testname-profile",
-            "description": "Base example for 'testname' user profile",
-            "kwargs": {
-                "username": "testname",
-                "email": "testmail@gmail.com",
-                "fullname": "Test Name"
-            }
-        },
-        {
-            "case": "osint",
-            "name": "johndoe-profile",
-            "description": "Osint example for 'johndoe' user profile",
-            "kwargs": {
-                "username": "johndoe",
-                "email": "johndoe@gmail.com",
-                "fullname": "John Doe"
-            }
-        },
-        {
-            "case": "recon",
-            "name": "facebook-website",
-            "description": "Recon example for 'facebook.com' website",
-            "kwargs": {
-                "url": "https://facebook.com",
-            }
-        },
-        {
-            "case": "recon",
-            "name": "habr-website",
-            "description": "Recon example for 'habr.com' website",
-            "kwargs": {
-                "url": "https://habr.com",
-            }
-        },
-        {
-            "case": "recon",
-            "name": "8-8-8-8-host",
-            "description": "Recon example for '8.8.8.8' host",
-            "kwargs": {
-                "ip": "8.8.8.8"
-            }
-        },
-        {
-            "case": "recon",
-            "name": "13-91-95-74-host",
-            "description": "Recon example for '13.91.95.74' host",
-            "kwargs": {
-                "ip": "13.91.95.74"
-            }
-        },
-    ]
+    CASES = []
 
 
-if __name__ == "__main__":
-    # fmt: off
-    logger.info(f"Start framework for {len(DefaultValues.CASES)} cases")
-
-    # Define CaseManager class
-    manager = CaseManager(cases=DefaultValues.CASES, max_workers=5)
-
-    # Run all the cases in parallel way
-    multiple_results = manager.multi_case_runner()
-    for single_result in multiple_results:
-        pprint(single_result)
-
-    # Run single case
-    single_results = manager.single_case_runner(
+def run_single_case(manager: CaseManager) -> dict or list:
+    """
+    Run single case
+    :param manager: manager to use
+    :return: data
+    """
+    return manager.single_case_runner(
         case_class="recon",
         case_name="test single runner",
         case_description="Nothing special",
         url="https://habr.com/"
     )
 
-    # Show results
-    pprint(single_results)
+
+def load_scenario(scenario: str or None = None) -> list:
+    """
+    Load scenario to run
+    :param scenario: scenario .yaml filename
+    :return: list with data
+    """
+    if not scenario:
+        return DefaultValues.CASES
+    try:
+        with open(scenario, mode="r") as scenario_file:
+            return safe_load(scenario_file)
+    except:
+        logger.error(msg=f"Scenario file is not available or can not be opened")
+
+
+if __name__ == "__main__":
+    # fmt: off
+
+    # Load scenario file
+    scenario_cases = load_scenario(scenario=argv[1] if len(argv) >= 2 else None)
+    if not scenario_cases:
+        logger.error(msg=f"Scenario file is empty. Cases is not defined")
+        exit(1)
+
+    # Start processing
+    logger.info(f"Start framework for {len(scenario_cases)} cases")
+
+    # Define CaseManager class
+    manager = CaseManager(cases=scenario_cases, max_workers=5)
+
+    # Run all the cases in parallel way
+    multiple_results = manager.multi_case_runner()
+    for single_result in multiple_results:
+        pprint(single_result)
+
     # fmt: on
